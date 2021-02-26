a8614857b63327cbf93ec7358a6421e10f62dd1f
Elle Hanson <elleh3113@gmail.com>
2021-02-25 19:21:10 -0500

    fixed some things, wrote function to inspect interpolation error

--- saturnLs.py
+++ saturnLs.py
@@ -2,9 +2,19 @@ from io import StringIO
 import numpy as np
 import pandas as pd
 
-"""
+__doc__ = """
 Functions to convert Saturn's solar longitude to and from SCET/UNIX timestamp, datetime, and Julian date.
-Written 2021 by LE Hanson
+
+Values:
+ * Ls: Solar longitude, apparent longitude of the sun seen from Saturn as defined in JPL Horizons.
+ * SY: Saturn year number where SY 1 is the Saturn year starting September 21, 1950.
+ * Ls2: Solar longitude since the beginning of SY 1 starting September 21, 1950.
+ * SCET: Time in seconds since January 1, 1970 (UNIX epoch).
+ * JD (or JDUT): Julian Date, UT.
+ * datetime: Python datetime object (including numpy datetime64, pandas Timestamp, and astropy Time).
+ * datestr (or datestring): Date and/or time as a string.
+
+Written 2021 by LE Hanson.
 """
 
 
@@ -19,36 +29,82 @@ __all__ = [
 
 # calendar and JD date of start of several "Saturn years"
 _sy_list = dict((
-    (0, {"date":"1921-04-12 00:26:26.287291050", "JDUT":2422791.5183598064}),
-    (1, {"date":"1950-09-21 19:02:45.138775468", "JDUT":2433546.293577995}),
-    (2, {"date":"1980-03-03 16:12:48.102177799", "JDUT":2444302.175556738}),
-    (3, {"date":"2009-08-11 02:04:25.416794062", "JDUT":2455054.586405287}),
-    (4, {"date":"2039-01-22 19:51:58.221873760", "JDUT":2465811.3277571974}),
-    (5, {"date":"2068-06-29 08:45:59.446648121", "JDUT":2476561.865271373}),
-    (6, {"date":"2097-12-13 07:54:51.884071350", "JDUT":2487320.829767177}),
-    (7, {"date":"2127-05-21 00:44:15.628328323", "JDUT":2498070.530736439}),
-    (8, {"date":"2156-11-05 03:53:00.718159676", "JDUT":2508831.6618138677}),
+    (-1, {"date":"1891-10-30 07:07:31.777902603", "JDUT":2412035.7968955776}),
+    (0,  {"date":"1921-04-12 00:26:22.458478928", "JDUT":2422791.5183154917}),
+    (1,  {"date":"1950-09-21 19:02:40.934159994", "JDUT":2433546.2935293308}),
+    (2,  {"date":"1980-03-03 16:12:43.076112986", "JDUT":2444302.175498566}),
+    (3,  {"date":"2009-08-11 02:04:21.114835262", "JDUT":2455054.5863554957}),
+    (4,  {"date":"2039-01-22 19:51:53.897613049", "JDUT":2465811.327707148}),
+    (5,  {"date":"2068-06-29 08:45:54.395126820", "JDUT":2476561.8652129066}),
+    (6,  {"date":"2097-12-13 07:54:47.293023586", "JDUT":2487320.82971404}),
+    (7,  {"date":"2127-05-21 00:44:11.711929321", "JDUT":2498070.53069111}),
+    (8,  {"date":"2156-11-05 03:52:56.824035645", "JDUT":2508831.661768797}),
     ))
 
 # DataFrame holding the date each "Saturn year" begins
 dfsy = pd.DataFrame.from_dict(_sy_list, orient='index').rename_axis(index="SY")
 dfsy['date'] = pd.to_datetime(dfsy['date'])
 
-#ephem_file = "saturn-from-titan-50_70.txt"
-ephem_file = "saturn-Ls-50_70.txt"
+# ephemeris files from JPL Horizons
+ephem_file_coarse = "saturn-Ls-1890_2160-sol_sparse.txt"
+ephem_file_fine = "saturn-Ls-1970_2040-sol.txt"
+# define default horizons output file to load
+ephem_file = ephem_file_coarse
 
+# csv ephemeris files, reduced from Horizons output
+ephem_csv_coarse = "saturn-Ls-sparse.csv"
+ephem_csv_fine = "saturn-Ls.csv"
+# define default csv file to load
+ephem_csv = ephem_csv_fine
+
+###################################
+# Independent conversion functions
+###################################
+
+# these functions do not rely on the ephemeris data
+
+# Ls2 and Ls, SY
 def Ls2_to_Ls(ls2):
+    '''Convert Ls2 to Ls.'''
     SY = ls2//360
     return ls2 - 360*SY
 
 def Ls2_to_SYLs(ls2):
+    '''Convert Ls2 to "Saturn year" SY and Ls.'''
     SY = ls2//360 + 1
     return SY, ls2 - 360*(SY-1)
 
 def SYLs_to_Ls2(Ls, SY=3):
+    '''Convert "Saturn year" SY and Ls to Ls2.'''
     return (SY-1)*360 + Ls
 
+
+# convert SCET and datetime
+def SCET_to_datetime(scet):
+    '''Convert SCET (seconds since UNIX epoch) to datetime'''
+    return pd.to_datetime(scet, unit='s')
+
+def pddt_scet(pddt):
+    '''Convert python datetime to SCET.
+    This function strips any timezone information.'''
+    return (pddt - pd.to_datetime("1970")).total_seconds()
+pddt_scet = np.frompyfunc(pddt_scet, nin=1, nout=1) 
+
+def datetime_to_SCET(pdt):
+    '''Convert datetime to SCET'''
+    from datetime import datetime
+    if isinstance(pdt, datetime):
+        pdt = pdt.replace(tzinfo=None)
+    return pddt_scet(pd.to_datetime(pdt))
+datetime_to_SCET = np.frompyfunc(datetime_to_SCET, nin=1, nout=1) 
+
+
+###########################
+# Ephemeris file functions
+###########################
+
 def scan_ephem(fname):
+    '''Scan a JPL Horizons ephemeris file (csv format) and return the csv data.'''
     import re
     with open(fname, "r") as fin:
         lines = fin.readlines()
@@ -69,59 +125,81 @@ def scan_ephem(fname):
     return "".join(txt).replace(", ,", ", NaN,")
 
 def parse_ephem(s):
+    """Parse the csv portion of JPL Horizon output (from scan_ephem) and return as a pandas DataFrame."""
     df = pd.read_csv(StringIO(s), header=0, na_values="n.a.")
-    #df.columns = df.columns.str.strip(" ()").str.translate(str.maketrans(" -():","_____")).str.replace("__","_")
     df = df.dropna("columns")
     if "Calendar_Date_TDB" in df:
         df["Calendar_Date_TDB"] = pd.to_datetime(df["Calendar_Date_TDB"].str[6:])
     if "Date_UT_HRMNSCfff" in df:
         df["Date_UT_HRMNSCfff"] = pd.to_datetime(df["Date_UT_HRMNSCfff"])
+    if "x1_way_down_LT" in df:
+        df["x1_way_down_LT"] = pd.to_timedelta(df["x1_way_down_LT"], 'min')
+        df = df.rename(columns={"x1_way_down_LT":"one_way_LT"})
     return df[[c for c in df.columns if "Unnamed" not in c]]
 
+
+def adjust_LT(df):
+    """Account for light time.
+    Adjust for distance of observer to Saturn using the column "one_way_LT"."""
+    from scipy.interpolate import interp1d
+    dfa = df.copy()
+    lt = dfa['one_way_LT'] # one-way light time
+    # calculate JDUT adjusted for light time
+    jdnew = dfa['JDUT'] - lt.dt.total_seconds()/86400 # correction to JDUT
+    ls2 = dfa["Ls2"]
+    # interpolate Ls2
+    finterp = interp1d(jdnew, ls2, kind='linear', fill_value='extrapolate')
+    ls2new = finterp(dfa["JDUT"]) # calculate interpolated Ls2
+    dls2 = ls2new - ls2 # difference in original Ls2 and new Ls2
+    dfa["Ls2"] = ls2new # replace Ls2 with corrected values
+    dfa["Ls"] += dls2 # correct Ls
+    # remove any nan's due to interpolation
+    dfa = dfa.loc[dfa[["Ls2", "Ls"]].dropna().index]
+    # return without one_way_LT
+    return dfa.drop(columns=['one_way_LT'])
+
+
 def make_df_Ls2SY(df):
+    """Calculate Ls2, SY, and SCET, referenced from "Saturn year" zero starting in 1921.
+    Input is a DataFrame including fields ['date', 'JDUT', 'Ls']."""
     dfa = pd.concat((df.copy(), dfsy.reset_index())).sort_values('JDUT')
-    #dfa = df.copy().merge(dfsy.reset_index()[['JDUT','date','SY']], how='outer',
-    #                    on=['date','JDUT'], sort=True)
     dfa["SY"] = dfa["SY"].ffill()
-    dfa = dfa.dropna().reset_index(drop=True)
+    # if beyond SY table, estimate year
+    # could use Ls to find actual start of year but this is easy
+    if np.any(dfa.JDUT < dfsy.JDUT.min()) or np.any(dfa.JDUT > dfsy.JDUT.max()+10750):
+        jdsy0 = 2433546.2935293308 # SY 0 julian date
+        jdyear = 10755 # julian days in saturn year
+        lx = (dfa.JDUT < dfsy.JDUT.min()) | (dfa.JDUT > dfsy.JDUT.max()+10750)
+        # estimate SY
+        dfa.loc[lx, "SY"] = np.floor((dfa.loc[lx, "JDUT"] - jdsy0)/jdyear) + 1
+    dfa = dfa.set_index('JDUT').truncate(df.JDUT.min(), df.JDUT.max()).reset_index()
+    #dfa = dfa.dropna(thresh=2).reset_index(drop=True)
     dfa["Ls2"] = dfa["Ls"] + 360*(dfa["SY"]-1)
     dfa["SCET"] = (dfa.date - pd.to_datetime("1970")).dt.total_seconds()
+    if 'one_way_LT' in dfa:
+        dfa = adjust_LT(dfa)
     return dfa[['date','JDUT','SCET','Ls2','Ls','SY']]
 
 
 def make_Ls_df2(df):
+    """Make the reference DataFrame from the ephemeris DataFrame."""
     df = df.rename(columns={"Date_UT_HRMNSCfff":"date",
                                  "App_Lon_Sun":"Ls",
                                  "Date_JDUT":"JDUT"})
     return make_df_Ls2SY(df)
 
-def make_Ls_df(df):
-    # find first Ls zero-crossing in DataFrame to find beginning Saturn year
-    # should be 9-22-1950
-    # everything prior is negative
-    lxneg = df.index < np.nonzero((df["App_Lon_Sun"] < 1).values)[0][0]
-    
-    dfa = df.set_index("Date_UT_HRMNSCfff")["App_Lon_Sun"].reset_index()
-    dfa["Ls2"] = dfa["App_Lon_Sun"]
-    dfa["JDUT"] = df.Date_JDUT
-    dfa["SCET"] =  (dfa.Date_UT_HRMNSCfff - pd.to_datetime("1970")).dt.total_seconds()
-    dfa.loc[lxneg, "Ls2"] += -360
-    
-    for ix in np.nonzero((dfa["Ls2"].diff().abs() > 300).values)[0]:
-        lx = dfa.index >= ix
-        dfa.loc[lx, "Ls2"] = dfa.loc[lx, "Ls2"] + 360
-    dfa["SY"] = (dfa["Ls2"]//360 + 1).astype('int8') # saturn year
-
-    dfa = dfa.rename(columns={"Date_UT_HRMNSCfff":"date", "App_Lon_Sun":"Ls"})
-    return dfa
-
 def load_ephem(fname=ephem_file):
+    '''load JPL HORIZONS csv output as a DataFrame'''
     return make_Ls_df2(parse_ephem(scan_ephem(fname)))
 
-def save_csv(df, fname="saturn-Ls.csv"):
+def save_csv_ephem(df, fname="saturn-Ls.csv"):
+    '''Save ephemeris DataFrame as a csv file.
+    This file loads significantly faster and is smaller than the raw Horizons output.'''
     df.drop(columns=["Ls2", "SY", "SCET"])[["date", "JDUT", "Ls"]].\
       to_csv(fname, index=False)
-def load_Ls_csv(fname="saturn-Ls.csv"):
+      
+def load_Ls_csv(fname=ephem_csv):
+    '''Loads the ephemeris csv file saved by `save_csv_ephem`.'''
     dfa = pd.read_csv(fname, parse_dates=[0], infer_datetime_format=True, cache_dates=False)
     return make_df_Ls2SY(dfa)
     dfa["SCET"] = (dfa.date - pd.to_datetime("1970")).dt.total_seconds()
@@ -129,79 +207,94 @@ def load_Ls_csv(fname="saturn-Ls.csv"):
     dfa["SY"] = dfa["SY"].astype('int8')
     return dfa
 
+# load the ephemeris data (Horizons output or csv)
+# this DataFrame is used in all of the Ls-time conversion functions by default
 #dfa = load_ephem(ephem_file)
 dfa = load_Ls_csv()
 
+#######################
+# conversion functions
+#######################
+
+
 # convert Ls2 and SCET
 def SCET_to_Ls2(scet, dfa=dfa):
+    """Convert SCET to Ls2."""
     if np.any(scet > dfa.SCET.max()) or np.any(scet < dfa.SCET.min()):
         raise ValueError("SCET outside of range in data table.")
     return np.interp(scet, dfa.SCET, dfa.Ls2)
+
 def Ls2_to_SCET(ls2, dfa=dfa):
+    """Convert Ls2 to SCET."""
     if np.any(ls2 > dfa.Ls2.max()) or np.any(ls2 < dfa.Ls2.min()):
         raise ValueError("Ls2 outside of range in data table.")
     return np.interp(ls2, dfa.Ls2, dfa.SCET)
 
+
 # convert Ls2 and JD
 def JD_to_SCET(jd, dfa=dfa):
+    """Convert JD to SCET."""
     if np.any(jd > dfa.JDUT.max()) or np.any(jd < dfa.JDUT.min()):
         raise ValueError("JD outside of range in data table.")
     return np.interp(jd, dfa.JDUT, dfa.SCET)
+
 def SCET_to_JD(scet, dfa=dfa):
+    """Convert SCET to JD."""
     if np.any(scet > dfa.SCET.max()) or np.any(scet < dfa.SCET.min()):
         raise ValueError("SCET outside of range in data table.")
     return np.interp(scet, dfa.SCET, dfa.JDUT)
 
-# convert SCET and datetime
-def SCET_to_datetime(scet):
-    return pd.to_datetime(scet, unit='s')
-def pddt_scet(pddt):
-    return (pddt - pd.to_datetime("1970")).total_seconds()
-pddt_scet = np.frompyfunc(pddt_scet, nin=1, nout=1) 
-def datetime_to_SCET(pdt):
-    from datetime import datetime
-    if isinstance(pdt, datetime):
-        pdt = pdt.replace(tzinfo=None)
-    return pddt_scet(pd.to_datetime(pdt))
-datetime_to_SCET = np.frompyfunc(datetime_to_SCET, nin=1, nout=1) 
-
 
 # convert datetime and Ls2
 def datetime_to_Ls2(pdt):
+    """Convert datetime to Ls2."""
     return SCET_to_Ls2(datetime_to_SCET(pdt))
-datetime_to_Ls2 = np.frompyfunc(datetime_to_Ls2, nin=1, nout=1) 
-def datetime_to_Ls(pdt, include_SY=False):
-    sy, ls = Ls2_to_SYLs(SCET_to_Ls2(datetime_to_SCET(pdt)))
-    if include_SY:
-        return sy, ls
+datetime_to_Ls2 = np.frompyfunc(datetime_to_Ls2, nin=1, nout=1)
+
+def datetime_to_SYLs(pdt):
+    """Convert datetime to (SY, Ls)."""
+    return Ls2_to_SYLs(SCET_to_Ls2(datetime_to_SCET(pdt)))
+datetime_to_SYLs = np.frompyfunc(datetime_to_SYLs, nin=1, nout=2) 
+
+def datetime_to_Ls(pdt):
+    """Convert datetime to Ls and SY."""
+    sy, ls = datetime_to_SYLs(pdt)
     return ls
 datetime_to_Ls = np.frompyfunc(datetime_to_Ls, nin=1, nout=1) 
+
+
 def Ls2_to_datetime(Ls2):
+    """Convert Ls2 to datetime."""
     return SCET_to_datetime(Ls2_to_SCET(Ls2))
-def SYLs_to_datetime(ls, sy=3):
+
+def SYLs_to_datetime(ls, sy=3, dfa=dfa):
+    """Convert SY and Ls to daetime."""
     return SCET_to_datetime(Ls2_to_SCET(SYLs_to_Ls2(sy, ls)))
+
 
 # convert date string to Ls
 def datestr_to_Ls2(datestr):
-    """
-    THIS FUNCION IS NOT GOOD....
-    I think the issue here is with datetime assuming local time.
-    """
-    from warnings import warn
-    warn("The function datestr_to_Ls2 is inaccurate")
+    """Convert datestring to Ls2."""
     return datetime_to_Ls2(pd.to_datetime(datestr))
+
 def datestr_to_Ls(datestr, include_SY=False):
+    """Convert datestring to Ls.
+    Returns Ls alone, or (SY, Ls) if include_SY is True."""
     sy, ls = Ls2_to_SYLs(datestr_to_Ls2(datestr))
     if include_SY:
         return sy, ls
     return ls
+
+
+
+
 #
 # horizons@ssd.jpl.nasa.gov
 # the funcions in this file use output from NASA Horizons using the following
 '''
 !$$SOF
 COMMAND= '699'
-CENTER= '500@606'
+CENTER= '500@10'
 MAKE_EPHEM= 'YES'
 TABLE_TYPE= 'OBSERVER'
 START_TIME= '1950-01-01'
@@ -220,7 +313,7 @@ R_T_S_ONLY= 'NO'
 REF_SYSTEM= 'J2000'
 CSV_FORMAT= 'YES'
 OBJ_DATA= 'YES'
-QUANTITIES= '44'
+QUANTITIES= '21,44'
 !$$EOF
 '''
 
--- test_saturnLs.py
+++ test_saturnLs.py
@@ -42,6 +42,64 @@ def t12():
     '''SYLs_to_Ls2'''
     return (SYLs_to_Ls2(dfa.Ls, dfa.SY) - dfa.Ls2)/dfa.Ls2.sum()
 
-fcs = [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12]
-for func in fcs:
-    print(f"{func.__doc__}: ", np.sum(func()))
+
+def plot_error_SCET_Ls2(ixskip=100, ixoff=0, dfa=dfa):
+    import matplotlib.pyplot as plt
+    ixbuf = max(ixskip,ixoff)*2
+    dfp = dfa.iloc[ixbuf:-ixbuf] # keep all rows
+    dfs = dfa.iloc[ixoff::ixskip] # only keep every ixskip rows
+    ls2 = SCET_to_Ls2(dfp.SCET, dfa=dfs) # calculate interpolated Ls2
+    dLs2 = dfp.Ls2.diff() # daily change in Ls2
+    ferr = (ls2-dfp.Ls2)/dLs2
+    rms = np.sqrt(np.mean(ferr**2))
+    # plot error as fraction of daily change in Ls2
+    plt.figure()
+    plt.plot(dfp.SCET, ferr, '.', markersize=4)
+    plt.title(f"interpolating over {ixskip} days; rms: {rms:0.3g}; max: {np.abs(ferr).max():0.3g}")
+    plt.xlabel("SCET [s]")
+    plt.ylabel("Ls2 error as fraction of daily $\Delta$Ls2")
+    return rms, np.abs(ferr).max()
+
+def plot_error_Ls2_SCET(ixskip=100, ixoff=0, dfa=dfa):
+    import matplotlib.pyplot as plt
+    ixbuf = max(ixskip,ixoff)*2
+    dfp = dfa.iloc[ixbuf:-ixbuf] # keep all rows
+    dfs = dfa.iloc[ixoff::ixskip] # only keep every ixskip rows
+    scet = Ls2_to_SCET(dfp.Ls2, dfa=dfs) # calculate interpolated SCET
+    dSCET = dfp.SCET.diff() # daily change in SCET
+    ferr = (scet-dfp.SCET)/dSCET
+    rms = np.sqrt(np.mean(ferr**2))
+    # plot error as fraction of daily change in SCET
+    plt.figure()
+    plt.plot(dfp.Ls2, ferr, '.', markersize=4)
+    plt.title(f"interpolating over {ixskip} days; rms: {rms:0.3g}; max: {np.abs(ferr).max():0.3g}")
+    plt.xlabel("Ls2 [$\degree$]")
+    plt.ylabel("SCET error as fraction of daily $\Delta$SCET")
+    return ferr
+
+def plot_error_trend(ixoff=0):
+    import matplotlib.pyplot as plt
+    errs = []
+    #ixs = np.concatenate((np.arange(2,10), np.logspace(1,np.log10(600),50).astype(int),[100,200,300]))
+    ixs = np.concatenate((np.arange(2,100), np.arange(100,300,2), np.arange(300,750,25)))
+    for ix in np.sort(ixs):
+        r,x = plot_error_SCET_Ls2(ix, ixoff=ixoff)
+        plt.close()
+        errs.append((ix,r,x))
+    errs = np.array(errs).T
+    plt.figure()
+    plt.plot(errs[0], errs[1], 'v--', label="rms rel err", markersize=3)
+    plt.plot(errs[0], errs[2], 'o--', label="max abs rel err", markersize=3)
+    plt.axhline(y=1, color='k', linestyle='-', linewidth=1)
+    plt.axhline(y=0.5, color='k', linestyle='--', linewidth=1)
+    plt.axhline(y=0.2, color='k', linestyle=':', linewidth=1)
+    plt.xlabel('interval')
+    plt.ylabel('fractional daily (24 hr) error')
+    plt.legend()
+    plt.xlim(40,400)
+    plt.ylim(0.03,1.4)
+
+def run_tests():
+    fcs = [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12]
+    for func in fcs:
+        print(f"{func.__doc__}: ", np.sum(func()))
